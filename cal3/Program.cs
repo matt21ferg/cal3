8688404fb027c5b49bb42ac611556d4f093dbe59
matt21ferg <matt21ferguson@gmail.com>
2022-01-05 21:10:57 -0600

    calulater

---
+++ cal3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace cal3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var num1 = GitNumber();
+            var isAddition = GitOperator();
+            var num2 = GitNumber();
+            if (isAddition)
+            {
+                Console.WriteLine(num1+num2);
+
+            }
+            else
+            {
+                Console.WriteLine(num1-num2);
+            }
+
+        }
+        public static int GitNumber()
+        {
+            Console.WriteLine("\nenter a number");
+            var num1 = Console.ReadLine();
+            return int.Parse(num1);
+        }
+        public static bool GitOperator() 
+        { 
+        Console.WriteLine("enter - for subtraction + for addtion");
+            var opperater =Console.ReadKey();
+            var o = opperater.KeyChar;
+            if(o == '+')
+            {
+                return true;
+            }
+            return false;
+        } 
+    }
+}
